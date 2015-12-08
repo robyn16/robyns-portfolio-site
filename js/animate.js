352cd53b52114439ccfa558faddc531cd274d843
robyn16 <robyn.asquini@gmail.com>
2015-12-08 14:28:10 -0500

    edits to home page

--- js/animate.js
+++ js/animate.js
@@ -1,7 +1,23 @@
-(function($){
 
-$('.tlt').textillate();
-$('.tlt').textillate({ in: { effect: 'rollIn' } });
+$(function () {
+  var $texts = $('.verticalscroll span'),
+      $container = $texts.find('.text-container'),
+      textheight = $texts.height(),
+      textCount = $texts.size(),
+      margins = 30;
 
+  function slideUp () {
+    $texts.animate({ 'bottom': textheight+margins}, 1000, function(){
+      var firstText =  $texts.eq(0).clone();
+      $('.verticalscroll').append(firstText);
+      $texts.eq(0).remove();
+      setTimeout(function(){
+        slideUp(); // recursively loop.
+      }, 1000);
+    });
+  }
 
-}
+  slideUp(); // trigger slideUp on page load.
+
+});
+
