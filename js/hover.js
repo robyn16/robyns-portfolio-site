3154144e69433a28db5ac1bc63693931b325118b
robyn16 <robyn.asquini@gmail.com>
2015-12-16 18:06:29 -0500

    home page

---
+++ js/hover.js
@@ -0,0 +1,12 @@
+ $('.piece img').each(function(i, img) {
+    
+      var currentImgHeight = $(img).height();
+      var currentImgWidth = $(img).width();
+      
+      // set piece
+      $(img).parent().height(currentImgHeight);
+      $(img).parent().width(currentImgWidth);
+      
+      // set caption
+      $(img).next().height(currentImgHeight);
+ });
