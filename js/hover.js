70697ecc61e60e1467f088d4f63b8af651d089e9
robyn16 <robyn.asquini@gmail.com>
2015-12-16 20:33:06 -0500

    fixed sizing of images on index page

--- js/hover.js
+++ js/hover.js
@@ -1,11 +1,11 @@
  $('.piece img').each(function(i, img) {
     
       var currentImgHeight = $(img).height();
-      var currentImgWidth = $(img).width();
+      // var currentImgWidth = $(img).width();
       
       // set piece
       $(img).parent().height(currentImgHeight);
-      $(img).parent().width(currentImgWidth);
+      // $(img).parent().width(currentImgWidth);
       
       // set caption
       $(img).next().height(currentImgHeight);
